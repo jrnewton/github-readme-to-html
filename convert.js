990657c5e997e94b31f6adf7313d1b0d5ccf0292
Jon Newton <newt@hey.com>
2021-03-16 08:09:25 -0400

    Add editor configs

--- convert.js
+++ convert.js
@@ -1,12 +1,12 @@
-var showdown  = require('showdown');
-var fs = require('fs-extra');
-let filename = "README.md"
-let pageTitle = process.argv[2] || "README"
+const showdown = require('showdown');
+const fs = require('fs-extra');
+const filename = 'README.md';
+let pageTitle = process.argv[2] || 'README';
 
 fs.readFile(__dirname + '/style.css', function (err, styleData) {
   fs.readFile(process.cwd() + '/' + filename, function (err, data) {
     if (err) {
-      throw err; 
+      throw err;
     }
     let text = data.toString();
 
@@ -17,36 +17,41 @@ fs.readFile(__dirname + '/style.css', function (err, styleData) {
       tables: true
     });
 
-    let preContent = `
+    let preContent =
+      `
     <!DOCTYPE html>
     <html>
       <head>
-        <title>` + pageTitle + `</title>
+        <title>` +
+      pageTitle +
+      `</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
       </head>
       <body>
         <div id='content'>
-    `
+    `;
 
-    let postContent = `
-
+    let postContent =
+      `
         </div>
-        <style type='text/css'>` + styleData + `</style>
+        <style type='text/css'>` +
+      styleData +
+      `</style>
       </body>
     </html>`;
 
-    html = preContent + converter.makeHtml(text) + postContent
+    html = preContent + converter.makeHtml(text) + postContent;
 
     converter.setFlavor('github');
     //console.log(html);
 
-    let filePath = process.cwd() + "/dist/README.html";
-    fs.writeFile(filePath, html, { flag: "wx" }, function(err) {
+    let filePath = process.cwd() + '/dist/README.html';
+    fs.writeFile(filePath, html, { flag: 'wx' }, function (err) {
       if (err) {
         console.log("File '" + filePath + "' already exists. Aborted!");
       } else {
-        console.log("Done, saved to " + filePath);
+        console.log('Done, saved to ' + filePath);
 
         fs.copySync(process.cwd() + '/assets', process.cwd() + '/dist/assets');
       }
